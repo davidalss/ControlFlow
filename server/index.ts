b72f370da279b656edb6980882e7a9d34c54298b
David Alisson <david.alisson92@Outlook.com>
2025-08-18 12:23:05 -0300

    fix: add CORS configuration to allow frontend requests

--- server/index.ts
+++ server/index.ts
@@ -6,6 +6,20 @@ import { storage } from "./storage";
 import { addRequestId, requestLogger } from "./lib/logger";
 
 const app = express();
+
+// CORS configuration
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
